dca93413fa6884f832742ae5f4bf763f732fe920
tanneratkin <tmanatkin@gmail.com>
2025-06-25 12:50:11 -0600

    added logout continued

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,7 @@ import { logout } from "./auth/actions";
 export default function RootPage() {
   return (
     <div>
-      <Link href="/auth/signup">Sign Up</Link>
-      <br />
-      <Link href="/auth/login">Login</Link>
+      <button onClick={() => logout()}>logout</button>
     </div>
   );
 }
