c8b651f90b37115dc424a988de274b3cc4a9efdc
tanneratkin <tmanatkin@gmail.com>
2025-12-17 12:23:25 -0700

    updated to new supabase API key names

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -3,8 +3,8 @@ import { cookies } from "next/headers";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+const supabaseServiceRoleKey = process.env.SUPABASE_SECRET_KEY;
 
 export function createServersideAdminClient() {
   return createClient(supabaseUrl!, supabaseServiceRoleKey!);
